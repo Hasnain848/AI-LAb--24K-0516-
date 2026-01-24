51bad59ae98ba4321fff1aa9d63c4b593f68d400
Hasnain848 <muhammadhasnainsiddiqui596@gmail.com>
2026-01-24 17:33:46 +0500

    Update Q1.py

--- Lab 01/Q1.py
+++ Lab 01/Q1.py
@@ -1,1 +1,12 @@
-t
+name= input("Enter Student name: ")
+marks= int(input("Enter Marks: "))
+print("Student Name: ",name)
+print("Marks: ",marks)
+if marks >= 85:
+  print("Grade: A")
+elif marks >= 70:
+  print("Grade: B")
+elif marks >= 50:
+  print("Grade: C")
+elif marks < 50:
+  print("Grade: Fail")
