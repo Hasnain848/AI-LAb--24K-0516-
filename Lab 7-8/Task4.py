b3f697739ca6f128e4653c1052be116c80f8dd85
Hasnain848 <muhammadhasnainsiddiqui596@gmail.com>
2026-03-25 22:14:34 +0500

    Add Task4.py for constraint programming example

---
+++ Lab 7-8/Task4.py
@@ -0,0 +1,22 @@
+from ortools.sat.python import cp_model
+
+model = cp_model.CpModel()
+
+num_vals=4
+
+A=model.new_int_var(0,num_vals-1,"A")
+B=model.new_int_var(0,num_vals-1,"B")
+C=model.new_int_var(0,num_vals-1,"C")
+
+model.add(A!=B)
+model.add(B!=C)
+model.add(A+B<=4)
+
+solver=cp_model.CpSolver()
+status=solver.solve(model)
+if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+    print(f"A = {solver.value(A)}")
+    print(f"B = {solver.value(B)}")
+    print(f"C = {solver.value(C)}")
+else:
+    print("No solution found.")
